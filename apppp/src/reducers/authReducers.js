e1421992fa7958154c502de64c59b86b9749c4c2
maayanyit <60900943+maayanyit@users.noreply.github.com>
2020-05-17 19:58:22 +0300

    Create authReducers.js

---
+++ apppp/src/reducers/authReducers.js
@@ -0,0 +1,29 @@
+import { SET_CURRENT_USER, USER_LOADING } from "../actions/types";
+const isEmpty = require("is-empty");
+const initialState = {
+  isAuthenticated: false,
+  user: {},
+  loading: false
+};
+export default function(state = initialState, action) {
+  // console.log("auth reducer1:");
+
+  // console.log(action.payload);
+  // console.log(state);
+
+  switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
+    case USER_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
+  }
+}
